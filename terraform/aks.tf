991a88e79fdccd1551ac018ba6496bc037c48cc3
hamzakalech <hamza.kalech@esprit.tn>
2025-03-13 13:03:04 +0000

    add aks.tf

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -1,1 +1,132 @@
+# Variables for better flexibility
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  default     = "hamza-resources"
+}
 
+variable "location" {
+  description = "Azure region for resources"
+  default     = "East US"
+}
+
+variable "cluster_name" {
+  description = "Name of the AKS cluster"
+  default     = "hamzaDevOps"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  default     = "1.28"
+}
+
+# Provider configuration
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = {
+    Environment = "Development"
+    ManagedBy   = "Terraform"
+  }
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "aks-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Subnet for AKS
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# AKS Cluster with system node pool
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.cluster_name
+  kubernetes_version  = var.kubernetes_version
+
+  # System node pool configuration
+  default_node_pool {
+    name                = "agentpool"
+    vm_size             = "Standard_B2als_v2"
+    node_count          = 1
+    # Auto-scaling settings should work in this version
+    enable_auto_scaling = true
+    min_count           = 1
+    max_count           = 2
+    os_disk_size_gb     = 30
+    os_sku              = "Ubuntu"
+    vnet_subnet_id      = azurerm_subnet.aks_subnet.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    network_policy    = "calico"
+    load_balancer_sku = "standard"
+  }
+
+  # For AzureRM v4.x, use these flags
+  role_based_access_control_enabled = true
+  azure_active_directory_role_based_access_control {
+    managed = true
+    azure_rbac_enabled = false
+  }
+}
+
+# User Node Pool (worker)
+resource "azurerm_kubernetes_cluster_node_pool" "worker" {
+  name                  = "worker"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  vm_size               = "Standard_B2als_v2"
+  enable_auto_scaling   = true
+  min_count             = 1
+  max_count             = 2
+  os_disk_size_gb       = 30
+  os_type               = "Linux"
+  os_sku                = "Ubuntu"
+  vnet_subnet_id        = azurerm_subnet.aks_subnet.id
+  mode                  = "User"
+  availability_zones    = [1, 2, 3]
+}
+
+# Outputs
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
+
+output "host" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  sensitive = true
+  description = "The Kubernetes server URL for remote access"
+}
+
+output "fqdn" {
+  value = azurerm_kubernetes_cluster.aks.fqdn
+  description = "The FQDN of the AKS cluster's API server"
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
