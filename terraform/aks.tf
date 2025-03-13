84ae8b46975ede47a4d29603160f0574d89f35a3
hamzakalech <hamza.kalech@esprit.tn>
2025-03-13 12:05:02 +0000

    update terraform

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -1,30 +1,76 @@
+# Variables for better flexibility
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  default     = "hamza-resources"
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  default     = "East US"
+}
+
+variable "cluster_name" {
+  description = "Name of the AKS cluster"
+  default     = "hamzaDevOps"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  default     = "1.28"
+}
+
+# Provider configuration
 provider "azurerm" {
   features {}
 }
 
-# 1. Create a Resource Group
+# Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "hamza-resources"
-  location = "East US"
+  name     = var.resource_group_name
+  location = var.location
+  tags     = {
+    Environment = "Development"
+    ManagedBy   = "Terraform"
+  }
 }
 
-# 2. Create the AKS Cluster
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "hamzaDevOps"
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "aks-vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "hamzaDevOps"
+  address_space       = ["10.0.0.0/16"]
+}
 
-  kubernetes_version  = "1.28" # Change to the latest available if needed
+# Subnet for AKS
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
 
+# AKS Cluster with Cost-Optimized System Node Pool
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.cluster_name
+  kubernetes_version  = var.kubernetes_version
+
+  # Using more cost-effective VM size for system pool
   default_node_pool {
-    name                = "systempool"
-    node_count          = 1
-    enable_auto_scaling = false
-    vm_size             = "Standard_B2als_v2" # Will be updated dynamically
-    os_disk_size_gb     = 16
+    name                = "agentpool"
+    vm_size             = "Standard_B2als_v2"
+    enable_auto_scaling = true
+    min_count           = 1
+    max_count           = 2
+    os_disk_size_gb     = 30
     os_sku              = "Ubuntu"
     vnet_subnet_id      = azurerm_subnet.aks_subnet.id
+    node_labels = {
+      "role" = "system"
+    }
   }
 
   identity {
@@ -32,8 +78,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   network_profile {
-    network_plugin = "azure"
-    network_policy = "none"
+    network_plugin    = "azure"
+    network_policy    = "calico"
     load_balancer_sku = "standard"
   }
 
@@ -44,17 +90,40 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-# 3. Create Virtual Network (if needed)
-resource "azurerm_virtual_network" "vnet" {
-  name                = "aks-vnet"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
+# User Node Pool (worker)
+resource "azurerm_kubernetes_cluster_node_pool" "worker" {
+  name                  = "worker"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  vm_size               = "Standard_B2als_v2"
+  enable_auto_scaling   = true
+  min_count             = 1
+  max_count             = 2
+  os_disk_size_gb       = 30
+  os_type               = "Linux"
+  os_sku                = "Ubuntu"
+  vnet_subnet_id        = azurerm_subnet.aks_subnet.id
+  mode                  = "User"
+  availability_zones    = [1, 2, 3]
+  node_labels = {
+    "role" = "worker"
+  }
 }
 
-resource "azurerm_subnet" "aks_subnet" {
-  name                 = "aks-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+# Outputs
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
+
+output "host" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  sensitive = true
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
 }
