ac60912405d4d5e7b205f5f04f0a3e4e45342922
hamzakalech <115652132+hamzakalech@users.noreply.github.com>
2025-04-08 17:40:24 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
+
+provider "kubernetes" {
+  host                   = module.aks.kube_config_host
+  client_certificate     = base64decode(module.aks.kube_config_client_certificate)
+  client_key             = base64decode(module.aks.kube_config_client_key)
+  cluster_ca_certificate = base64decode(module.aks.kube_config_cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.aks.kube_config_host
+    client_certificate     = base64decode(module.aks.kube_config_client_certificate)
+    client_key             = base64decode(module.aks.kube_config_client_key)
+    cluster_ca_certificate = base64decode(module.aks.kube_config_cluster_ca_certificate)
+  }
+}
+
+# Modules
 
 module "resource_group" {
   source              = "./modules/resource_group"
@@ -23,6 +58,21 @@ module "aks" {
   subnet_id           = module.network.subnet_id
 }
 
+resource "null_resource" "update_kubeconfig" {
+  depends_on = [module.aks]
+
+  provisioner "local-exec" {
+    command = <<EOT
+az aks get-credentials --resource-group ${module.resource_group.name} --name ${module.aks.cluster_name} --overwrite-existing
+sleep 5
+EOT
+  }
+}
+
 module "monitoring" {
-  source = "./modules/monitoring"
+  source     = "./modules/monitoring"
+  depends_on = [null_resource.update_kubeconfig]
+  providers = {
+    helm = helm
+  }
 }
