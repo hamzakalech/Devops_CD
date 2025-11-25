fdad2b2707529707380a6672843a1a0835d0d84c
hamzakalech <115652132+hamzakalech@users.noreply.github.com>
2025-11-25 15:46:00 +0100

    Delete terraform directory

--- terraform/modules/aks/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "cluster_name" {}
-variable "kubernetes_version" {}
-variable "resource_group_name" {}
-variable "location" {}
-variable "subnet_id" {}
--- terraform/modules/resource_group/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-
-  tags = {
-    Environment = "Development"
-    ManagedBy   = "Terraform"
-  }
-}
